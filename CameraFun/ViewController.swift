64d4aedcd7f83481795f6a5f7e8058b1d72e9723
Guilherme Ribeiro <guilherme.rib@terra.com.br>
2015-11-26 11:58:47 -0200

    Commit

--- CameraFun/ViewController.swift
+++ CameraFun/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var openCameraButton: UIButton!
+    @IBOutlet var openLibraryButton: UIButton!
+    
+    @IBOutlet var imageDisplay: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func libraryButtonAction(sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+    }
 
+    @IBAction func cameraButtonAction(sender: UIButton!){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .Camera
+        presentViewController(picker, animated: true, completion: nil)
+
+   }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage;
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
 
